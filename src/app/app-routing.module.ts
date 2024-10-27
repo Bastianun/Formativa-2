1ba62adf199aaa129e5413da25a4f28ac6764888
Kazu <bastian.zarate.ayala@cftmail.cl>
2024-10-27 12:34:35 -0300

    primer commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './componentes/inicio/inicio.component';
+import { UbicanosComponent } from './componentes/ubicanos/ubicanos.component';
+import { ContactanosComponent } from './componentes/contactanos/contactanos.component';
+
+const routes: Routes = [
+  {
+    path:"Inicio",
+    component:InicioComponent
+  },
+  {
+    path:"Ubicanos",
+    component:UbicanosComponent
+  },
+  {
+    path:"Contactanos",
+    component:ContactanosComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
